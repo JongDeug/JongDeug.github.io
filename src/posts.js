3e48dbab65c36cbcf18552bdd85a3898bee832f8
JongDeug(linux) <pinpoint1998@gmail.com>
2022-06-25 19:56:36 +0900

    add bootstrap-icon cdn

--- src/posts.js
+++ src/posts.js
@@ -31,7 +31,8 @@ const posthtml = data => {
     <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="/node_modules/bootstrap-icons/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
+    
     <!-- CSS -->
     <link rel="stylesheet" href="/css/styles.css">
 
